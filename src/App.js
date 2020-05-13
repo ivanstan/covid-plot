f4b12dcf21c36cca9de94c88db151d1717d82ee4
Ivan Stanojevic <ivan.stanojevic@protonmail.com>
2020-05-13 20:44:16 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    async getJson(url) {
+        const result = await fetch(url);
+        return await result.json();
+    }
+
+    componentDidMount() {
+        Promise.all([this.getJson('measures.json'), this.getJson('stingency.json')]).then((a, b) => {
+
+            console.log(a, b);
+
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
